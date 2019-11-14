4e3b5160e6b1ce5e1feab60414be404f1e82a2a6
Denis BOUVET <denis.bouvet@live.com>
2019-11-14 19:12:51 +0100

    Suppression des tag XML

--- ComplexObjectToXmlFile/Form1.cs
+++ ComplexObjectToXmlFile/Form1.cs
@@ -23,16 +23,19 @@ namespace ComplexObjectToXmlFile
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
 
             Employees Emps = new Employees();
             // Note that only the collection is serialized -- not the
             // CollectionName or any other public property of the class.
             Emps.CollectionName = "Employees";
-            Employee John100 = new Employee("John", "100xxx");
+            Employee John100 = new Employee("Johnny", "100xxx");
             Emps.Add(John100);
             XmlSerializer x = new XmlSerializer(typeof(Employees));
             TextWriter writer = new StreamWriter(new FileStream("foo.xml",FileMode.Create),Encoding.UTF8);
-            x.Serialize(writer, Emps);
+            x.Serialize(writer, Emps,ns);
+
         }
     }
 }
